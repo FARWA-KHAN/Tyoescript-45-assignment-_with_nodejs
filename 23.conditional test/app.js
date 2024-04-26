acebb68b5623d3f76ce399d1144183d63bea7477
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:58:35 +0500

    Add files via upload

---
+++ 23.conditional test/app.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// making a variabales
+var five = 5;
+var ten = 10;
+//test 1
+console.log("Test 1 : Five is equal to 5 ?");
+console.log(five == 5);
+//test 2
+console.log("\nTest 2 : 10 is equal to 10");
+console.log(ten == 10);
+//test 3
+console.log("\nTest 3: 5 is not equal to 10");
+console.log(five != ten);
+//test 4
+console.log("\nTest 4 : 10 is greater than 5");
+console.log(ten > 5);
+//test 5 
+console.log("\Test 5 : 5 is smaller than 10");
+console.log(five < 10);
+//test 6
+console.log("\Test 6 : 10 is smaller than 5");
+console.log(ten < 5);
+//test 7
+console.log("\Test 7 : 5 is equal to 1o");
+console.log(five == ten);
+//test 8
+console.log("\Test 8 : 10 is not equal to 10");
+console.log(ten != 10);
+//test 9
+console.log("\Test 9 : 5 is smaller than 10");
+console.log(five > 10);
+//test 10
+console.log("\Test 10 : 100 is smaller than 50");
+console.log(100 < 50);
