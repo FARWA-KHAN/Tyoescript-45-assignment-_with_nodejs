9d05ffc360adbe0641f2d2131d1d65bd49ab5374
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:59:31 +0500

    Add files via upload

---
+++ 26.alien color 2/main.js
@@ -0,0 +1,14 @@
+var aliencolor = "green";
+if (aliencolor === "green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+//second version
+if (aliencolor === "blue") {
+    console.log("I come from if  statment");
+}
+else {
+    console.log("I come from Else statment");
+}
