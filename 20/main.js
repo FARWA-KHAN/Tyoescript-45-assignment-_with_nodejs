0f465bdaa30b7212b303c005ce4a73b766ddddbf
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:57:27 +0500

    Add files via upload

---
+++ 20/main.js
@@ -0,0 +1,6 @@
+// making a programming laanguages array
+var programmingLanguages = ["Typescript", "Javascript", "Python", "PhP"];
+//print the message of list
+console.log("List of Programming language:");
+// print the values of array in the form of list
+programmingLanguages.forEach(function (laguage) { return console.log(laguage); });
---
+++ 20/main.ts
@@ -0,0 +1,8 @@
+// making a programming laanguages array
+let programmingLanguages: string[] = ["Typescript", "Javascript", "Python", "PhP"];
+
+//print the message of list
+console.log("List of Programming language:");
+
+// print the values of array in the form of list
+programmingLanguages.forEach(laguage =>console.log(laguage));
