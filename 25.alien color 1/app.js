2edf161036b2d8029706c9f83d5b4dabc453f2d1
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:59:20 +0500

    Add files via upload

---
+++ 25.alien color 1/app.js
@@ -0,0 +1,9 @@
+//let variable
+var aliencolor = "White";
+if (aliencolor === "Black") {
+    //code  black
+    console.log("Alien color is black player just earned 5 points");
+}
+if (aliencolor === "White") {
+    console.log("Alien color is white");
+}
---
+++ 25.alien color 1/app.ts
@@ -0,0 +1,11 @@
+//let variable
+let aliencolor = "White";
+
+if (aliencolor === "Black"){
+    //code  black
+    console.log("Alien color is black player just earned 5 points");
+}
+
+if (aliencolor === "White"){
+    console.log("Alien color is white");
+}
