eecac0410bb81088a80db580d5d71250894debd1
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:55:21 +0500

    Add files via upload

---
+++ 14.Guest List/app.js
@@ -0,0 +1,4 @@
+var Guest_List = ['Babar Azam', 'Shadab khan', 'Rizwan'];
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
+}
