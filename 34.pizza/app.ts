ba4b6251d384ad5e3ce75c1cd9e6c62af4287452
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 19:01:50 +0500

    Add files via upload

---
+++ 34.pizza/app.ts
@@ -0,0 +1,8 @@
+let pizza = ["chicken tikka", "malai cheese", "fajita"]
+
+for (let onePizza of pizza){
+    console.log(`I like ${onePizza} pizza`);
+}
+
+console.log("pizza is love")
+console.log("I eat pizza")
