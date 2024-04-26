bf3c5a705682141b180367b8cdcb8cbf39b323ce
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:57:14 +0500

    Add files via upload

---
+++ 19.dinner guest/app.js
@@ -0,0 +1,5 @@
+var Guest_List = ['Babar Azam', 'Shadab khan', 'Rizwan'];
+//for(let i=0; i<Guest_List.length; i++){
+//console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')};
+var lengthGuests = Guest_List.length;
+console.log("We are inviting total ".concat(lengthGuests, " guests,"));
---
+++ 19.dinner guest/app.ts
@@ -0,0 +1,7 @@
+let Guest_List = ['Babar Azam','Shadab khan','Rizwan'];
+
+ //for(let i=0; i<Guest_List.length; i++){
+    //console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')};
+
+let lengthGuests : number = Guest_List.length;
+console.log(`We are inviting total ${lengthGuests} guests,`);
