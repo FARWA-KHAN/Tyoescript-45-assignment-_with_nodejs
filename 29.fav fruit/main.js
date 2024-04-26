79ec3012556b591916d987af527dfd5514e130e4
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 19:00:08 +0500

    Add files via upload

---
+++ 29.fav fruit/main.js
@@ -0,0 +1,17 @@
+// creating a array
+var favorite_fruit = ["Mango", "Strawberry", "orange"];
+if (favorite_fruit.includes("Mango")) {
+    console.log("I really like Mangos");
+}
+if (favorite_fruit.includes("Strawberry")) {
+    console.log("I really like Strawberrys");
+}
+if (favorite_fruit.includes("orange")) {
+    console.log("I really like orange");
+}
+if (favorite_fruit.includes("Apple")) {
+    console.log("I really like apples");
+}
+if (favorite_fruit.includes("Banana")) {
+    console.log("I really like banana");
+}
---
+++ 29.fav fruit/main.ts
@@ -0,0 +1,18 @@
+// creating a array
+let favorite_fruit = ["Mango", "Strawberry", "orange"]
+
+if (favorite_fruit.includes("Mango")){
+    console.log("I really like Mangos");
+}
+if (favorite_fruit.includes("Strawberry")){
+    console.log("I really like Strawberrys");
+}
+if (favorite_fruit.includes("orange")){
+    console.log("I really like orange");
+}
+if (favorite_fruit.includes("Apple")){
+    console.log("I really like apples");
+}
+if (favorite_fruit.includes("Banana")){
+    console.log("I really like banana");
+}
