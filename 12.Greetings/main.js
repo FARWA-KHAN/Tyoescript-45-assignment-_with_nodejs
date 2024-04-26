d9667822788133dcf676811b70b2233f78808e81
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:54:34 +0500

    Add files via upload

---
+++ 12.Greetings/main.js
@@ -0,0 +1,2 @@
+var friendName = ["farwa", "famia", "riyab", "mahum"];
+friendName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ", How are you ?")); });
---
+++ 12.Greetings/main.ts
@@ -0,0 +1,2 @@
+let friendName = ["farwa", "famia", "riyab", "mahum"];
+friendName.forEach(friendName => console.log(`Hello ${friendName}, How are you ?`));
