74ec92a6190721dfbf23208e9d34b9e558a84d9d
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:57:49 +0500

    Add files via upload

---
+++ 21/app.ts
@@ -0,0 +1,13 @@
+interface itCourse {
+    courseName : string;
+    location : string;
+    onSiteStudents : number;
+}
+
+let itCourse ={
+    courseName : "Typescript and javascript",
+    location : "Governor House Sindh",
+    onsiteStudent : 50000
+};
+
+console.log(itCourse);
