015cb92e6da7694505369dc6cf33a352084f23fa
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 19:02:01 +0500

    Add files via upload

---
+++ 35.animals/app.ts
@@ -0,0 +1,7 @@
+let petAnimal =["cat","dog", "rabbit"]
+
+for (let onepet of petAnimal){
+    console.log(`A ${onepet} would make a great pet`)
+}
+
+console.log("Any of these animals would make a great pet!~")
