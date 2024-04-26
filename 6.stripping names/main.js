5225f4247346ddf715446b7b5438fd23f51853e8
Farwa khan <khanfarwa392@gmail.com>
2024-04-26 18:52:07 +0500

    Add files via upload

---
+++ 6.stripping names/main.js
@@ -0,0 +1,4 @@
+var persoName = '    \n\tFarwa Khan\t\n    ';
+console.log(persoName);
+var without_Whitespace = persoName.trim();
+console.log(without_Whitespace);
---
+++ 6.stripping names/main.ts
@@ -0,0 +1,8 @@
+
+const persoName :string = '    \n\tFarwa Khan\t\n    ';
+
+console.log(persoName);
+
+const without_Whitespace :string = persoName.trim();
+
+console.log(without_Whitespace);
